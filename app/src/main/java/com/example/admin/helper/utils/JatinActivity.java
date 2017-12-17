357c715513f6c286d10561dd4e1b8c6f53d25460
Admin <jatinsharma3399@gmail.com>
2017-12-16 18:02:10 -0800

    hiii it

---
+++ app/src/main/java/com/example/admin/helper/utils/JatinActivity.java
@@ -0,0 +1,15 @@
+package com.example.admin.helper.utils;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.admin.helper.R;
+
+public class JatinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jatin);
+    }
+}
