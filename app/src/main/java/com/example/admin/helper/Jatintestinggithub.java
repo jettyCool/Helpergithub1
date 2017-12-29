7e7535df4c585e2fc6b830f279aff97f0af8e29b
Admin <jatinsharma3399@gmail.com>
2017-12-28 23:35:20 -0800

    jatintesting file to github

---
+++ app/src/main/java/com/example/admin/helper/Jatintestinggithub.java
@@ -0,0 +1,13 @@
+package com.example.admin.helper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Jatintestinggithub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jatintestinggithub);
+    }
+}
